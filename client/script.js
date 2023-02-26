c757eef52149a67b21a408e6bc89eeb455f6df9b
adeniyi <ogunlana.adeniyi01@gmail.com>
2023-02-26 22:44:19 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
 
-    const response = await fetch('https://alexai-t0qc.onrender.com', {
+    const response = await fetch('https://alexai-eoce.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
